2d46fc6abff84d2cd038904478f60113082578e8
Augusto Perin <60995721+AugustoPerin@users.noreply.github.com>
2025-03-13 18:48:55 -0300

    Update Content.tsx

--- components/Content.tsx
+++ components/Content.tsx
@@ -4,27 +4,34 @@ export default function Content() {
   return (
     <div className={styles.content}>
       <h1>Para Minha Querida Lua</h1>
-      <p>Assim como a lua ilumina a noite, você ilumina meu dia-a-dia.</p>
-      <p>Assim como a lua ilumina a noite, você ilumina meu dia-a-dia.</p>
-      <p>Em cada olhar, encontro um brilho que me guia,</p>
-      <p>Seu sorriso acende a chama do meu coração,</p>
-      <p>Transformando cada momento em doce emoção.</p>
-      <p></p>
-      <p>Você é a Lua que clareia minha estrada,</p>
-      <p>A poesia viva que transforma a jornada,</p>
-      <p>Em meio à rotina, és a luz que irradia,</p>
-      <p>Trazendo paz e magia a cada novo dia.</p>
-      <p></p>
-      <p>No reflexo sereno do céu e do mar,</p>
-      <p>Vejo a beleza do nosso amor a brilhar,</p>
-      <p>E no compasso suave do tempo a passar,</p>
-      <p>Sei que juntos podemos sempre sonhar.</p>
-      <p></p>
-      <p>Com o coração pulsando em doce sintonia,</p>
-      <p>Te convido a trilhar essa estrada de alegria.</p>
-      <p>Lua, minha amada, minha eterna inspiração:</p>
-      <p>Quer namorar comigo e ser a Lua da minha constelação?</p>
+      
+      <div className={styles.stanza}>
+        <p>Assim como a lua ilumina a noite, você ilumina meu dia-a-dia.</p>
+        <p>Em cada olhar, encontro um brilho que me guia,</p>
+        <p>Seu sorriso acende a chama do meu coração,</p>
+        <p>Transformando cada momento em doce emoção.</p>
+      </div>
+      
+      <div className={styles.stanza}>
+        <p>Você é a Lua que clareia minha estrada,</p>
+        <p>A poesia viva que transforma a jornada,</p>
+        <p>Em meio à rotina, és a luz que irradia,</p>
+        <p>Trazendo paz e magia a cada novo dia.</p>
+      </div>
+      
+      <div className={styles.stanza}>
+        <p>No reflexo sereno do céu e do mar,</p>
+        <p>Vejo a beleza do nosso amor a brilhar,</p>
+        <p>E no compasso suave do tempo a passar,</p>
+        <p>Sei que juntos podemos sempre sonhar.</p>
+      </div>
+      
+      <div className={styles.stanza}>
+        <p>Com o coração pulsando em doce sintonia,</p>
+        <p>Te convido a trilhar essa estrada de alegria.</p>
+        <p>Lua, minha amada, minha eterna inspiração:</p>
+        <p>Quer namorar comigo e ser a Lua da minha constelação?</p>
+      </div>
     </div>
   )
 }
-
